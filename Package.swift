8c7414baeb324b1e729f3a729d4695860a168671
Tim <0xtimc@gmail.com>
2017-02-13 19:12:48 +0000

    Add Project Skeleton

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "VaporSecurityHeaders",
+    dependencies: [
+    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Vapor-SecurityHeaders
+# VaporSecurityHeaders
 Harden Your Security Headers For Vapor
