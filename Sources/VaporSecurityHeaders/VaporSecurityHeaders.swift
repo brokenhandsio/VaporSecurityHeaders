3d8d2b1cad1a285a58df69c8cf04d59bfae1d91f
Tim <0xtimc@gmail.com>
2017-02-14 14:47:48 +0000

    Added default headers and headers for an API

--- Sources/VaporSecurityHeaders/VaporSecurityHeaders.swift
+++ Sources/VaporSecurityHeaders/VaporSecurityHeaders.swift
@@ -1,11 +1,52 @@
 import HTTP
 
 struct SecurityHeaders: Middleware {
+    
+    private let isApi: Bool
+    
+    init() {
+        isApi = false
+    }
+    
+    init(api: Bool) {
+        self.isApi = api
+    }
+    
+    
+    enum HeaderNames: String {
+        case cto = "X-Content-Type-Options"
+        case csp = "Content-Security-Policy"
+        case xfo = "X-Frame-Options"
+        case xssProtection = "X-XSS-Protection"
+    }
+    
+    
     func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
         
-        response.headers["X-Content-Type-Options"] = "nosniff"
+        response.headers[HeaderKey(HeaderNames.cto.rawValue)] = getHeader(for: .cto)
+        response.headers[HeaderKey(HeaderNames.csp.rawValue)] = getHeader(for: .csp)
+        response.headers[HeaderKey(HeaderNames.xfo.rawValue)] = getHeader(for: .xfo)
+        response.headers[HeaderKey(HeaderNames.xssProtection.rawValue)] = getHeader(for: .xssProtection)
         
         return response
+    }
+    
+    private func getHeader(for headerName: HeaderNames) -> String {
+        switch headerName {
+        case .cto:
+            return "nosniff"
+        case .csp:
+            if isApi {
+                return "default-src 'none'"
+            }
+            else {
+                return "default-src 'self'"
+            }
+        case .xfo:
+            return "deny"
+        case .xssProtection:
+            return "1; mode=block"
+        }
     }
 }
--- Tests/VaporSecurityHeadersTests/HeaderTests.swift
+++ Tests/VaporSecurityHeadersTests/HeaderTests.swift
@@ -8,21 +8,80 @@ import HTTP
 class HeaderTests: XCTestCase {
 
     static var allTests = [
-        ("testContentTypeOptionsHeaderSet", testContentTypeOptionsHeaderSet),
+        ("testDefaultHeaders", testDefaultHeaders),
+        ("testAllHeadersForApi", testAllHeadersForApi),
+        ("testAPIHeadersWithHSTS", testAPIHeadersWithHSTS),
+        ("testDefaultHeadersWithHSTS", testDefaultHeadersWithHSTS)
     ]
+    
+    private var request: Request!
+    
+    override func setUp() {
+        request  = try! Request(method: .get, uri: "/test/")
+    }
 
-    func testContentTypeOptionsHeaderSet() throws {
-        let expectedHeaderValue = "nosniff"
-        let drop = try makeTestDroplet()
-        let request = try! Request(method: .get, uri: "/test/")
+    func testDefaultHeaders() throws {
+        let expectedXCTOHeaderValue = "nosniff"
+        let expectedCSPHeaderValue = "default-src 'self'"
+        let expectedXFOHeaderValue = "deny"
+        let expectedXSSProtectionHeaderValue = "1; mode=block"
+        
+        let drop = try makeTestDroplet(middlewareToAdd: SecurityHeaders())
         let response = try drop.respond(to: request)
         
-        XCTAssertEqual(expectedHeaderValue, response.headers["X-Content-Type-Options"])
+        XCTAssertEqual(expectedXCTOHeaderValue, response.headers["X-Content-Type-Options"])
+        XCTAssertEqual(expectedCSPHeaderValue, response.headers["Content-Security-Policy"])
+        XCTAssertEqual(expectedXFOHeaderValue, response.headers["X-Frame-Options"])
+        XCTAssertEqual(expectedXSSProtectionHeaderValue, response.headers["X-XSS-Protection"])
     }
     
-    private func makeTestDroplet() throws -> Droplet {
+    func testAllHeadersForApi() throws {
+        let expectedXCTOHeaderValue = "nosniff"
+        let expectedCSPHeaderValue = "default-src 'none'"
+        let expectedXFOHeaderValue = "deny"
+        let expectedXSSProtectionHeaderValue = "1; mode=block"
+        
+        let drop = try makeTestDroplet(middlewareToAdd: SecurityHeaders(api: true))
+        let response = try drop.respond(to: request)
+        
+        XCTAssertEqual(expectedXCTOHeaderValue, response.headers["X-Content-Type-Options"])
+        XCTAssertEqual(expectedCSPHeaderValue, response.headers["Content-Security-Policy"])
+        XCTAssertEqual(expectedXFOHeaderValue, response.headers["X-Frame-Options"])
+        XCTAssertEqual(expectedXSSProtectionHeaderValue, response.headers["X-XSS-Protection"])
+    }
+    
+    func testAPIHeadersWithHSTS() throws {
+        
+    }
+    
+    /*
+    func testAPIHeadersWithHSTSandHPKP() throws {
+        
+    }
+    
+    func testAPIHeadersWithHPKP() throws {
+        
+    }
+    */
+    
+    func testDefaultHeadersWithHSTS() throws {
+        
+    }
+    
+    /*
+    func testDefaultHeadersWithHSTSandHPKP() throws {
+        
+    }
+    
+    func testDefaultHeadersWithHPKP() throws {
+        
+    }
+    */
+    
+    
+    private func makeTestDroplet(middlewareToAdd: Middleware) throws -> Droplet {
         let drop = Droplet(arguments: ["dummy/path/", "prepare"])
-        drop.middleware.append(SecurityHeaders())
+        drop.middleware.append(middlewareToAdd)
         
         drop.get("test") { req in
             return "TEST"
