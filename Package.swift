eb032b2d5965ab414e1d55f52c539f4410d44db3
Tim <0xtimc@gmail.com>
2017-04-11 21:15:20 +0100

    Get working in Vapor 2

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VaporSecurityHeaders",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
+    	.Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
     ]
 )
