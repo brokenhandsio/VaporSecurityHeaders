ca571ee137bc484382557835a391683570404378
Caleb Kinney <cakinney@gmail.com>
2019-02-14 06:13:08 -0500

    Mark ContentSecurityPolicy initializer and functions public

--- Sources/VaporSecurityHeaders/Configurations/ContentSecurityPolicyConfiguration.swift
+++ Sources/VaporSecurityHeaders/Configurations/ContentSecurityPolicyConfiguration.swift
@@ -68,82 +68,82 @@ public class ContentSecurityPolicy {
         return policy.joined(separator: "; ")
     }
 
-    func set(value: String) -> ContentSecurityPolicy {
+    public func set(value: String) -> ContentSecurityPolicy {
         policy.append(value)
         return self
     }
 
-    func baseUri(sources: String...) -> ContentSecurityPolicy {
+    public func baseUri(sources: String...) -> ContentSecurityPolicy {
         policy.append("base-uri \(sources.joined(separator: " "))")
         return self
     }
 
-    func blockAllMixedContent() -> ContentSecurityPolicy {
+    public func blockAllMixedContent() -> ContentSecurityPolicy {
         policy.append("block-all-mixed-content")
         return self
     }
 
-    func connectSrc(sources: String...) -> ContentSecurityPolicy {
+    public func connectSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("connect-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func defaultSrc(sources: String...) -> ContentSecurityPolicy {
+    public func defaultSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("default-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func fontSrc(sources: String...) -> ContentSecurityPolicy {
+    public func fontSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("font-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func formAction(sources: String...) -> ContentSecurityPolicy {
+    public func formAction(sources: String...) -> ContentSecurityPolicy {
         policy.append("form-action \(sources.joined(separator: " "))")
         return self
     }
 
-    func frameAncestors(sources: String...) -> ContentSecurityPolicy {
+    public func frameAncestors(sources: String...) -> ContentSecurityPolicy {
         policy.append("frame-ancestors \(sources.joined(separator: " "))")
         return self
     }
 
-    func frameSrc(sources: String...) -> ContentSecurityPolicy {
+    public func frameSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("frame-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func imgSrc(sources: String...) -> ContentSecurityPolicy {
+    public func imgSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("img-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func manifestSrc(sources: String...) -> ContentSecurityPolicy {
+    public func manifestSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("manifest-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func mediaSrc(sources: String...) -> ContentSecurityPolicy {
+    public func mediaSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("media-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func objectSrc(sources: String...) -> ContentSecurityPolicy {
+    public func objectSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("object-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func pluginTypes(types: String...) -> ContentSecurityPolicy {
+    public func pluginTypes(types: String...) -> ContentSecurityPolicy {
         policy.append("plugin-types \(types.joined(separator: " "))")
         return self
     }
 
-    func requireSriFor(values: String...) -> ContentSecurityPolicy {
+    public func requireSriFor(values: String...) -> ContentSecurityPolicy {
         policy.append("require-sri-for \(values.joined(separator: " "))")
         return self
     }
 
-    func reportTo(reportToObject: CSPReportTo) -> ContentSecurityPolicy {
+    public func reportTo(reportToObject: CSPReportTo) -> ContentSecurityPolicy {
         let encoder = JSONEncoder()
         guard let data = try? encoder.encode(reportToObject) else { return self }
         guard let jsonString = String(data: data, encoding: .utf8) else { return self }
@@ -151,33 +151,35 @@ public class ContentSecurityPolicy {
         return self
     }
 
-    func reportUri(uri: String) -> ContentSecurityPolicy {
+    public func reportUri(uri: String) -> ContentSecurityPolicy {
         policy.append("report-uri \(uri)")
         return self
     }
 
-    func sandbox(values: String...) -> ContentSecurityPolicy {
+    public func sandbox(values: String...) -> ContentSecurityPolicy {
         policy.append("sandbox \(values.joined(separator: " "))")
         return self
     }
 
-    func scriptSrc(sources: String...) -> ContentSecurityPolicy {
+    public func scriptSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("script-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func styleSrc(sources: String...) -> ContentSecurityPolicy {
+    public func styleSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("style-src \(sources.joined(separator: " "))")
         return self
     }
 
-    func upgradeInsecureRequests() -> ContentSecurityPolicy {
+    public func upgradeInsecureRequests() -> ContentSecurityPolicy {
         policy.append("upgrade-insecure-requests")
         return self
     }
 
-    func workerSrc(sources: String...) -> ContentSecurityPolicy {
+    public func workerSrc(sources: String...) -> ContentSecurityPolicy {
         policy.append("worker-src \(sources.joined(separator: " "))")
         return self
     }
+    
+    public init() {}
 }
