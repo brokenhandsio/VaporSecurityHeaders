6859a5cfa7974ece46be8b6e065e05b1d87f3725
Evan Hahn <me@evanhahn.com>
2022-04-12 21:13:58 -0500

    Add support for fallback referrer policies
    Co-authored-by: Tim Condon <0xTim@users.noreply.github.com>
    
    The `Referrer-Policy` header [supports fallback policies][0]. This adds
    support for that in this package!
    
    [0]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy#specify_a_fallback_policy

--- Sources/VaporSecurityHeaders/Configurations/ReferrerPolicyConfiguration.swift
+++ Sources/VaporSecurityHeaders/Configurations/ReferrerPolicyConfiguration.swift
@@ -2,7 +2,7 @@ import Vapor
 
 public struct ReferrerPolicyConfiguration: SecurityHeaderConfiguration {
 
-    public enum Options: String {
+    public enum Directive: String {
         case empty = ""
         case noReferrer = "no-referrer"
         case noReferrerWhenDowngrade = "no-referrer-when-downgrade"
@@ -14,13 +14,17 @@ public struct ReferrerPolicyConfiguration: SecurityHeaderConfiguration {
         case unsafeUrl = "unsafe-url"
     }
 
-    private let option: Options
+    private let directives: [Directive]
 
-    public init(_ option: Options) {
-        self.option = option
+    public init(_ directive: Directive) {
+        self.directives = [directive]
+    }
+
+    public init(_ directives: [Directive]) {
+        self.directives = directives
     }
 
     func setHeader(on response: Response, from request: Request) {
-        response.headers.replaceOrAdd(name: .referrerPolicy, value: option.rawValue)
+        response.headers.replaceOrAdd(name: .referrerPolicy, value: directives.map({ $0.rawValue }).joined(separator: ", "))
     }
 }
